2b43d09e40bb6c099cae8ab1adbe86a30a89e80d
volkbav <berezov86@gmail.com>
2025-11-30 16:52:21 +0300

    add admin

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Topics(models.Model):
+class Topic(models.Model):
     # Тема, которую изучает пользователь
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
@@ -9,3 +9,21 @@ class Topics(models.Model):
     def __str__(self):
         # Возвращает строковое представление модели.
         return self.text
+    
+class Entry(models.Model):
+    # """Информация, изученная пользователем по теме"""
+    # внешний ключ
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    # используется для создания доп. атрибута,
+    # который приказывает Django использовать форму множественного числа Entries
+    #  при обращении более чем к одной записи. 
+    # (Без этого Django будет использовать неправильную форму Entrys.)
+    class Meta:
+        verbose_name_plural = 'entries'
+        
+    def __str__(self):
+        # """Возвращает строковое представление модели."""
+        return f"{self.text[:50]}..."
