8473039529b39c7f119a8a2ec3b6de0dabc4e709
alex <berezov86@gmail.com>
2025-12-01 11:38:30 +0300

    update app "topics"

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,4 +8,6 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Домашняя страница
     path('', views.index, name='index'),
+    path('topics/', views.topics, name='topics'),
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
