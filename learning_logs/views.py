6c62e9ac233fdf3061a5aaaf478f928ca543bc02
alex <berezov86@gmail.com>
2025-12-02 14:09:01 +0300

    add validation

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 
 # Create your views here.
@@ -11,7 +12,7 @@ def index(request):
 
 @login_required
 def topics(request):
-    topics = Topic.objects.order_by("date_added")
+    topics = Topic.objects.filter(owner=request.user).order_by("date_added")
     context = {'topics': topics}
     return render(
         request,
@@ -23,6 +24,8 @@ def topics(request):
 def topic(request, topic_id):
     # """Выводит одну тему и все ее записи."""
     topic = Topic.objects.get(id=topic_id)
+    if topic.owner != request.user:
+        raise Http404
     entries = topic.entry_set.order_by('-date_added') # "-" - сортировка в обратном порядке
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
@@ -37,7 +40,9 @@ def new_topic(request):
         # Отправлены данные POST; обработать данные.
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_logs:topics')
         # Вывести пустую или недействительную форму.
     context = {'form': form}
@@ -62,10 +67,13 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     """Редактирует существующую запись."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    if topic.owner != request.user:
+        raise Http404
     
     if request.method != 'POST':
         # Исходный запрос; форма заполняется данными текущей записи.
